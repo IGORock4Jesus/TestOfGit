b374f537b59eee97c89452d854a85ac9a83832e5
Igor Kozlov <IGORock.guitar@outlook.com>
2018-07-31 09:55:22 +0600

    Добавлен класс Петр

---
+++ TestOfGit/Petr.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOfGit
+{
+	class Petr
+	{
+		public override string ToString()
+		{
+			return "Привет! Я Петр!";
+		}
+	}
+}
